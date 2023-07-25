65d101b43833b19d951f24e37e9d150e08d14636
ShardaJhagit <jhas1203@gmail.com>
2023-07-25 13:17:17 +0530

    testing
    testing

--- src/Show.js
+++ src/Show.js
@@ -103,8 +103,9 @@ function Show() {
             <div>
 
                 <div className=" h-screen  w-auto p-10  bg-gradient-to-r from-purple-500 to-pink-500 shadow-2xl shadow-slate-600 hover:border-zinc-950 hover: border-solid hover:border-2  " >
-                    <div className="bg-slate-600 text-2xl text-white">
+                    <div className="bg-slate-600 text-2xl text-white w-96 mx-auto ">
                     <h1>Weather Application</h1></div>
+                    
 
 
 
@@ -112,7 +113,8 @@ function Show() {
                         <div>
                             <img src={back} width="500px" height="500px" alt="bg" className="rounded-2xl"  id="i1"></img>
                         </div>
-                        <div className="ml-20 mt-32 ">
+                        <div className="ml-18 mt-10 ">
+                        <p >you can see the weather of any Location by enter the location <br></br>in the box and click on search button.</p>
                             <input type="text" placeholder="Search Your Location" onChange={e => setQuery(e.target.value)} value={query} className="p-1 border-2 border-black focus:outline-none focus:ring focus:ring-violet-300 rounded-xl"/>
                             <button onClick={search} className="border-2 border-black bg-gradient-to-r from-violet-500 to-fuchsia-500 p-1  w-40 ml-10 ">Search</button>
                             <div className="font-serif text-center text-3xl mx-auto mt-20 shadow-2xl shadow-slate-900 hover:border-zinc-950 hover: border-solid hover:border-2 w-48 bg-white">
@@ -123,8 +125,8 @@ function Show() {
                     </div>
                      
 
-                    <div className="flex flex-col">
-                        <div className="m-0 shadow-2xl shadow-slate-900 hover:border-zinc-950 hover: border-solid hover:border-2 w-50 mx-auto p-4 bg-white">
+                    <div className="flex ">
+                        <div className=" m-0 shadow-2xl shadow-slate-900 hover:border-zinc-950 hover: border-solid hover:border-2 w-50 mx-auto p-4 bg-white">
                         <div className="font-semibold text-xl font-sans"><Clk/></div>
                         <div className="mt-2 font-bold font-serif">{dateBuilder(new Date())}</div>
                         </div>
@@ -132,7 +134,7 @@ function Show() {
                         {(typeof weather.main != "undefined") ?
                             <>
                                 
-                                <div className="shadow-2xl shadow-slate-900 hover:border-zinc-950 hover: border-solid hover:border-2 w-48 mx-auto mt-10 p-4 bg-white">{temperature(weather)}</div></> : " "}</div>
+                                <div className="shadow-2xl shadow-slate-900 hover:border-zinc-950 hover: border-solid hover:border-2 w-48 mx-auto mt-8 p-4 bg-white">{temperature(weather)}</div></> : " "}</div>
 
                     </div>
                     
